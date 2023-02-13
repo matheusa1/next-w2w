e2042d81e2c895aaeb7d47126f0000556bd43c25
Matheus Andrade <2002matheus@gmail.com>
2023-02-13 18:24:13 -0300

    finish dark theme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="dark">
       <Head />
-      <body className="bg-blackBg">
+      <body className="dark:bg-blackBg bg-slate-100 darkT">
         <Main />
         <NextScript />
       </body>
---
+++ src/utils/switchTheme.ts
@@ -0,0 +1,13 @@
+export const switchTheme = () => {
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
+  } else {
+    document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
+  }
+};
