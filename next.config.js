d311ca49e072943e99378f3f8f5664a8d44dd496
Matheus Andrade <2002matheus@gmail.com>
2023-02-14 12:54:55 -0300

    add .env and axios

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    API_KEY: process.env.API_KEY,
+    SEARCH_TV: process.env.SEARCH_TV,
+    SEARCH_MOVIE: process.env.SEARCH_MOVIE,
+    IMAGE: process.env.IMAGE,
+    MOVIE: process.env.MOVIE,
+    TV: process.env.TV,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,14 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 
 const Home = (): ReactElement => {
+  const getData = async () => {
+    
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
       <h1>Home</h1>
