c7c3019de7c91be7a64cbf6e07dcbf69f43a5a67
Matheus Andrade <2002matheus@gmail.com>
2023-02-17 10:55:05 -0300

    add buttons

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,8 +2,13 @@ import Divisor from "@/components/Divisor";
 import { MoviesCarousel, SeriesCarousel } from "@/components/MoviesCarousel";
 import { movieProps, tvProps } from "@/types";
 import axios from "axios";
+import Image from "next/image";
 import { ReactElement, useEffect, useState } from "react";
 import "swiper/css";
+
+import Deku from "@/assets/deku.svg";
+import Miranha from "@/assets/miranha.svg";
+import Link from "next/link";
 
 const apiKey = process.env.API_KEY;
 const bestsMovies = process.env.BASE_URL;
@@ -48,6 +53,47 @@ const Home = (): ReactElement => {
               }
             }}
           />
+        </div>
+      </div>
+      <div className="mb-4">
+        <span className="darkT font-axiforma dark:text-white">Categorias</span>
+        <div className="flex h-36 gap-4">
+          <Link
+            href={"category"}
+            className="tm relative w-full cursor-pointer rounded-tm bg-linearCategoryBlue p-4 shadow-lg shadow-blue-300 transition-all duration-300 hover:scale-[1.01] dark:shadow-blue-900 md:rounded-tmMd md:px-8 lg:rounded-tmLg"
+          >
+            <Image
+              className="absolute top-0 -left-10 transition-all duration-1000 sm:left-10"
+              src={Miranha}
+              alt={""}
+            />
+            <div className="flex flex-col items-end text-white">
+              <h1 className="font-axiforma text-sm font-bold md:text-base">
+                Filmes
+              </h1>
+              <span className="font-axiforma text-[8px] md:text-xs">
+                + 850.000 títulos
+              </span>
+            </div>
+          </Link>
+          <Link
+            href={"category"}
+            className="relative w-full cursor-pointer rounded-tm2 bg-linearCategoryRed p-4 shadow-xl shadow-red-200 transition-all duration-300 hover:scale-[1.01] dark:shadow-red-900 md:rounded-tm2Md md:px-8 lg:rounded-tm2Lg"
+          >
+            <Image
+              className="absolute -top-8 -right-10 transition-all duration-1000 sm:right-10"
+              src={Deku}
+              alt={""}
+            />
+            <div className="flex flex-col text-white">
+              <h1 className="font-axiforma text-sm font-bold md:text-base">
+                Séries
+              </h1>
+              <span className="font-axiforma text-[8px] md:text-xs">
+                + 140.000 títulos
+              </span>
+            </div>
+          </Link>
         </div>
       </div>
       <div className="flex flex-col gap-2">
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,18 @@ module.exports = {
           "linear-gradient(94.97deg, rgba(25, 161, 190, 0.6) 20.87%, rgba(125, 65, 146, 0.6) 82.12%);",
         linearCard:
           "linear-gradient(180deg, rgba(0, 0, 0, 0) 41.67%, rgba(0, 0, 0, 0.75) 61.46%);",
+        linearCategoryBlue:
+          "linear-gradient(180deg, #16CAF1 0%, #0143A7 78.65%);",
+        linearCategoryRed:
+          "linear-gradient(179.71deg, #FF2E2E 27.14%, #E08939 99.75%);",
+      },
+      borderRadius: {
+        tm: "75% 25% 25% 25% / 40% 25% 25% 25%",
+        tmMd: "85% 15% 5% 5% / 20% 15% 15% 15%",
+        tmLg: "85% 5% 5% 5% / 20% 10% 15% 15%",
+        tm2: "25% 75% 25% 25% / 25% 40% 25% 25%",
+        tm2Md: "15% 85% 5% 5% / 15% 20% 15% 15%",
+        tm2Lg: "5% 85% 5% 5% / 10% 20% 15% 15%",
       },
     },
     fontFamily: {
