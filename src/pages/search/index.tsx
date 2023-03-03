98bf469cc8fd6391ae16f99b30de046c36158a4c
Matheus Andrade <2002matheus@gmail.com>
2023-03-03 09:38:12 -0300

    finish pagination

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,8 +1,12 @@
 import MainButton from "@/components/MainButton";
 import { SearchPageInput } from "@/components/MainInput";
 import ResultsWrapper from "@/components/ResultsWrapper";
+import { Pagination, PaginationItem } from "@mui/material";
+import { useWindowWidth } from "@react-hook/window-size";
 import axios from "axios";
 import { ReactElement, useCallback, useEffect, useState } from "react";
+
+import { BiLeftArrowAlt, BiRightArrowAlt } from "react-icons/bi";
 
 const getMoviesBySearchQuery = process.env.SEARCH_MOVIE;
 const getTvsBySearchQuery = process.env.SEARCH_TV;
@@ -14,10 +18,22 @@ const Search = (): ReactElement => {
   const [movies, setMovies] = useState();
   const [tvs, setTvs] = useState();
   const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const onlyWidth = useWindowWidth();
 
   const onCategoriesButtonClick = (category: "movie" | "tv") => {
     setActiveButton(category);
     localStorage.setItem("type", category);
+  };
+
+  const onPageChange = (e: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+    localStorage.setItem("page", value.toString());
+    window.scroll({
+      top: 0,
+      behavior: "smooth",
+    })
   };
 
   const getMoviesByQuery = useCallback(async () => {
@@ -30,9 +46,10 @@ const Search = (): ReactElement => {
               : getTvsBySearchQuery
           }?${API_KEY}&language=pt-BR&query=${
             searchQuery === "" ? "a" : searchQuery
-          }`
+          }&page=${page}`
         )
         .then((res) => {
+          setTotalPages(res.data.total_pages);
           if (activeButton === "movie") {
             setMovies(res.data.results);
           } else {
@@ -42,7 +59,7 @@ const Search = (): ReactElement => {
     } catch (error) {
       console.log(error);
     }
-  }, [searchQuery, activeButton]);
+  }, [activeButton, searchQuery, page]);
 
   useEffect(() => {
     localStorage.getItem("searchQuery") &&
@@ -93,6 +110,25 @@ const Search = (): ReactElement => {
       <ResultsWrapper
         data={activeButton === "movie" ? movies : tvs}
         type={activeButton}
+      />
+
+      <Pagination
+        className="mx-auto my-6"
+        size={`${
+          onlyWidth > 374 ? "medium" : onlyWidth > 700 ? "large" : "small"
+        }`}
+        count={totalPages}
+        color={"primary"}
+        variant="outlined"
+        page={page}
+        onChange={onPageChange}
+        renderItem={(item) => (
+          <PaginationItem
+            slots={{ previous: BiLeftArrowAlt, next: BiRightArrowAlt }}
+            className="darkT border-purple-300 hover:border-blue-500 hover:text-blue-500 dark:text-white"
+            {...item}
+          />
+        )}
       />
     </div>
   );
